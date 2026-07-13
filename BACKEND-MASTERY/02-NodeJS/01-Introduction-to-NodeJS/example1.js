c63af110ab0271c9bb68c7c7fcdf55cd0f61bad3
HAKUPA11 <ishnarsh1119@gmail.com>
2026-07-13 19:09:45 +0530

    done with the example1

---
+++ BACKEND-MASTERY/02-NodeJS/01-Introduction-to-NodeJS/example1.js
@@ -0,0 +1,10 @@
+console.log("hello node.js !")
+
+const name="harsh"
+console.log(`welcome ${name}`)
+
+const a=102
+const b =134
+console.log(`sum is ${a+b}`)
+
+// document.getElementById("title"); this will throw error because node.js  has no  DOM
